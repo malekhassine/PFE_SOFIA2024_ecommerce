9cb98ce9e15bd71dcbe1371ca5f48883c4e9eb2f
malekhassine <malek.hassine@esprit.tn>
2024-04-23 15:33:41 +0100

    first commit

---
+++ api-gateway/src/main/java/com/programming/techie/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//Added comment to test Jenkins Job.
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
